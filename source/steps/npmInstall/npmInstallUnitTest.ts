9258337981a2f70bbb4cf96b0757cd43fab5e9ef
Openbaringen <openbaringen@protonmail.com>
2017-12-11 08:12:52 +0200

    Refactoring npmInstall, towards #20

--- source/steps/npmInstall/npmInstallUnitTest.ts
+++ source/steps/npmInstall/npmInstallUnitTest.ts
@@ -7,59 +7,61 @@ import * as WebSocket from 'ws';
 import {spawn as nativeSpawn} from 'child_process';
 import {spawn} from "../../helpers/spawn/spawn";
 import {pushStreamThroughWebSocketConnections} from "../../helpers/pushStreamThroughWebsocketConnections/pushStreamThroughWebSocketConnections";
+import {setupServer} from "../../setup/setupServer";
+import {Server} from "http";
 
 const http = require('http');
-const server = http.createServer();
-
 const readFile = promisify(fs.readFile);
 const mkdir = promisify(fs.mkdir);
 const exists = promisify(fs.exists);
 
-describe('Given a package.json file with dependencies', function () {
-    this.timeout(40000);
-    let currentDir = process.cwd(),
-        testWorkingDirectory;
-
-    beforeEach( async function () {
-        testWorkingDirectory = `${currentDir}/../npmFakeProject`;
-        await mkdir(testWorkingDirectory);
-        await copyAsync(`${currentDir}/source/mocks/npmFakeProject`, `${testWorkingDirectory}`);
-        process.chdir(testWorkingDirectory);
-    });
-
-    describe('And a Web Socket Server', function () {
-        let wss: WebSocket.Server;
+describe('UNIT UNDER TEST: npmInstall', function () {
+    describe('Given a Web Socket Server', function () {
+        let wss: WebSocket.Server,
+            server: Server;
 
         beforeEach(async function () {
-            wss = new WebSocket.Server({server});
-
-            server.listen(process.env.PORT || 8999, () => {
-            });
+            ({server, wss} = await setupServer(WebSocket.Server, http.createServer))
         });
 
-        describe('When npm install is run', function () {
+        describe('And a npm project with package.json declaring its dependencies', function () {
+            this.timeout(40000);
+            let currentDir = process.cwd(),
+                testWorkingDirectory;
+
             beforeEach(async function () {
-                await npmInstall(run, spawn, nativeSpawn, pushStreamThroughWebSocketConnections, wss);
+                testWorkingDirectory = `${currentDir}/../npmFakeProject`;
+                await mkdir(testWorkingDirectory);
+                await copyAsync(`${currentDir}/source/mocks/npmFakeProject`, `${testWorkingDirectory}`);
+                process.chdir(testWorkingDirectory);
             });
 
-            it('Then it should install those dependencies', async function () {
-                const textBuffer = await readFile('package.json');
-                const {dependencies} = JSON.parse(textBuffer.toString());
-                const dependencyNames = Reflect.ownKeys(dependencies);
-                let promises = dependencyNames.map(async function(dependencyName) {
-                    expect(await exists(`node_modules/${dependencyName}`)).to.be.true
+
+            describe('When npm install is run', function () {
+                beforeEach(async function () {
+                    await npmInstall(run, spawn, nativeSpawn, pushStreamThroughWebSocketConnections, wss);
                 });
 
-                await Promise.all(promises);
+                it('Then it should install those dependencies', async function () {
+                    const textBuffer = await readFile('package.json');
+                    const {dependencies} = JSON.parse(textBuffer.toString());
+                    const dependencyNames = Reflect.ownKeys(dependencies);
+                    let promises = dependencyNames.map(async function (dependencyName) {
+                        expect(await exists(`node_modules/${dependencyName}`)).to.be.true
+                    });
+
+                    await Promise.all(promises);
+                });
             });
+
+
+            afterEach(function () {
+                process.chdir(currentDir);
+            })
         });
 
         afterEach(function () {
             server.close();
         })
     });
-
-    afterEach(function () {
-        process.chdir(currentDir);
-    })
 });
